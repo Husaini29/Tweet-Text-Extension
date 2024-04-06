90c5332ae77ae804e25a58c3b7c09976475b9de9
Husaini29 <Husainibohra86@gmail.com>
2024-04-06 21:22:06 +0530

    chrome extension

---
+++ content.js
@@ -0,0 +1,67 @@
+(function() {
+  
+  const observer = new MutationObserver(function(mutationsList, observer) {
+      for(let mutation of mutationsList) {
+          if (mutation.type === 'childList') {
+              const replyField = document.querySelector('.public-DraftStyleDefault-block');
+
+              if (replyField) {
+                  const div = document.createElement("div");
+                  div.className = "tweetBtnContainer"
+                  const button = document.createElement('button');
+                  button.textContent = 'Get Tweet';
+                  button.className = "tweetTextBtn";
+                  button.addEventListener('click', getOriginalTweetText);
+                  div.appendChild(button);
+
+                  replyField.parentNode.insertBefore(button, replyField.nextSibling);
+                  // Disconnect observer after finding reply field
+                  observer.disconnect();
+              }
+              break;
+          }
+      }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+
+  function getOriginalTweetText() {
+    
+      const replyField = document.querySelector('.public-DraftStyleDefault-block');
+      const placeHolder = document.querySelector(".public-DraftEditorPlaceholder-inner");
+
+
+      // Find the element containing original tweet text
+      const tweetContainer = document.querySelector('[data-testid="tweetText"]');
+      
+      if (tweetContainer) {
+          const originalTweetElement = tweetContainer.querySelector('[data-testid="tweetText"] > span');
+    
+          if (originalTweetElement) {
+              const originalTweetText = originalTweetElement.textContent.trim();
+  
+              alert(originalTweetText);
+
+              placeHolder.parentElement.removeChild(placeHolder);
+              
+              // Manipulate reply field as needed
+              const brTag = replyField.querySelector('span > br[data-text="true"]');
+
+              if(brTag){
+                const spanTag = document.createElement('span');
+                spanTag.dataset.text = "true";
+                spanTag.textContent = originalTweetText;
+
+                brTag.parentNode.replaceChild(spanTag,brTag);
+              }else{
+                console.log("No br tag found");
+              }
+
+          } else {
+              alert("Couldn't find the tweet text.");
+          }
+      } else {
+          alert("Original tweet container not found");
+      }
+  }
+})();
